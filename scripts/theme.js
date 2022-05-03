2d8d02ac51a88b9b67e462eb0ed14245e476ecba
ajs3801 <tt14an@gmail.com>
2022-05-03 23:50:22 +0900

    simple design of theme.html and addEventListener to button

---
+++ scripts/theme.js
@@ -0,0 +1,22 @@
+let NamecardType = "";
+
+let ModernButton = document.querySelector("#modern__select");
+ModernButton.addEventListener("click", () => {
+  NamecardType = "modern";
+  console.log(NamecardType);
+  window.location.href = "../index/Namecard.html";
+})
+
+let SimpleButton = document.querySelector("#simple__select");
+SimpleButton.addEventListener("click", () => {
+  NamecardType = "simple";
+  console.log(NamecardType);
+  window.location.href = "../index/Namecard.html";
+})
+
+let ColorButton = document.querySelector("#color__select");
+ColorButton.addEventListener("click", () => {
+  NamecardType = "colorful";
+  console.log(NamecardType);
+  window.location.href = "../index/Namecard.html";
+})
