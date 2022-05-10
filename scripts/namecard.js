af48d121ff2d13b67934573fdc579d6a04063575
Jeongwon Kim <jmjmjms08@gmail.com>
2022-05-10 20:37:01 +0900

    /bug

--- scripts/namecard.js
+++ scripts/namecard.js
@@ -145,4 +145,4 @@ rightbtn.addEventListener("click", () => {
 // let finish = document.querySelector("#finish__btn");
 // finish.addEventListener("click", () => {
 //   window.location.href = "../index/Result.html";
-})
+//})
