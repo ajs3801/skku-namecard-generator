ed26ba911743b593173d77cc22cb6f26ece5594f
ajs3801 <tt14an@gmail.com>
2022-05-10 18:02:50 +0900

    fix bug (can't insert in svg)

--- scripts/namecard.js
+++ scripts/namecard.js
@@ -25,54 +25,53 @@ let Namebtn = document.querySelector("#nameField__btn");
 Namebtn.addEventListener("click", () => {
   //delete text field
   let target = document.querySelector("#name__svg");
-  let result = document.querySelector("#result__name__svg");
+  //let result = document.querySelector("#result__name__svg");
 
   //make new element
   let Namefield = document.querySelector("#nameField");
   let Name = Namefield.value;
 
   name_js = Name;
-  result.textContent = Name;
+  //result.textContent = Name;
   target.textContent = Name;
 })
 
 let idbtn = document.querySelector("#idField__btn");
 idbtn.addEventListener("click", () => {
   let target = document.querySelector("#id__svg");
-  let result = document.querySelector("#result__id__svg");
+  //let result = document.querySelector("#result__id__svg");
 
   let idfield = document.querySelector("#idField");
   let id = idfield.value;
   
   id_js = id;
-  result.textContent = id;
+  //result.textContent = id;
   target.textContent = id;
 })
 
 let majorbtn = document.querySelector("#majorField__btn");
 majorbtn.addEventListener("click", () => {
   let target = document.querySelector("#major__svg");
-  let result = document.querySelector("#result__major__svg");
+  //let result = document.querySelector("#result__major__svg");
 
   let majorfield = document.querySelector("#majorField");
   let major = majorfield.value;
 
   major_js = major;
-  result.textContent = major;
+  //result.textContent = major;
   target.textContent = major;
 })
 
 let Emailbtn = document.querySelector("#EmailField__btn");
 Emailbtn.addEventListener("click", () => {
   let target = document.querySelector("#Email__svg");
-  let result = document.querySelector("#result__Email__svg");
+  //let result = document.querySelector("#result__Email__svg");
 
   let Emailfield = document.querySelector("#EmailField");
   let Email = Emailfield.value;
   Email_js = Email;
-  result.textContent = Email;
+  //result.textContent = Email;
   target.textContent = Email;
-
 })
 
 let finish = document.querySelector("#finish__btn");
