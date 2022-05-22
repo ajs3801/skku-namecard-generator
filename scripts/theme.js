e467abcb473cf45581a8592068f3305bd1020ac6
Jeongwon Kim <jmjmjms08@gmail.com>
2022-05-22 22:20:44 +0900

    theme.js documentaion

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,6 +1,9 @@
 let NamecardType = "";
 // module.exports.NamecardType = NamecardType;
 
+//These are a function of the button used to select a theme.
+
+//Move to Namecard_modern.html.
 let ModernButton = document.querySelector("#modern__select");
 ModernButton.addEventListener("click", () => {
   NamecardType = "modern";
@@ -8,6 +11,7 @@ ModernButton.addEventListener("click", () => {
   window.location.href = "../page/Namecard_modern.html";
 })
 
+//Move to Namecard_simple.html.
 let SimpleButton = document.querySelector("#simple__select");
 SimpleButton.addEventListener("click", () => {
   NamecardType = "simple";
@@ -15,6 +19,7 @@ SimpleButton.addEventListener("click", () => {
   window.location.href = "../page/Namecard_simple.html";
 })
 
+//Move to Namecard_colorful.html.
 let ColorButton = document.querySelector("#color__select");
 ColorButton.addEventListener("click", () => {
   NamecardType = "colorful";
@@ -22,3 +27,11 @@ ColorButton.addEventListener("click", () => {
   window.location.href = "../page/Namecard_colorful.html";
 })
 
+//If you want to add another theme, change "XXXX" of the code below.
+
+/*let XXXXButton = document.querySelector("#color__select");
+XXXXButton.addEventListener("click", () => {
+  NamecardType = "XXXX";
+  console.log(NamecardType);
+  window.location.href = "../page/Namecard_XXXX.html";
+})*/
